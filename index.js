ec2c7243c67d1915e9cdefebc4b9a73d753578b1
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-13 13:54:23 -0700

    try again

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ num = document.getElementById("numberBox");
 
 //create a way to convert any positive integer the user enters to a negative integer.
 
-//  function convertNumber(num) {
-//      num = document.getElementById("numberBox");
-//      return (-Math.abs(num));
-// }
+ function convertNumber(num) {
+     num = document.getElementById("numberBox");
+     -(Math.abs(3));
+}
 
-//  convertNumber(num.value);
+ convertNumber(num);
 
 
-console.log(-(Math.abs(3)));
