5d8c793ee37f8b2e82d7a66a3a6e08c2d1a3dbf5
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-13 13:41:43 -0700

    test out to make sure we're grabbing the element on the DOM

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ num = document.getElementById("numberBox");
 
 //create a way to convert any positive integer the user enters to a negative integer.
 
-function convertNumber(num) {
-    console.log(num);
-    return -Math.abs(num);
-}
+// function convertNumber(num) {
+//     num = document.getElementById("numberBox");
+//     return -Math.abs(num);
+// }
 
-convertNumber(88);
+// convertNumber(88);
+
+console.log(num);
