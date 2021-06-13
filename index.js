1d0c88dd1447f0aa3bea26c39c54e903695e23f1
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-13 14:07:23 -0700

    try to render on the page

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 //grab elements from the DOM
 
-num = document.getElementById("numberBox");
+let num = document.getElementById("numberBox");
+const goButton = document.getElementById("go-btn");
+const parentDiv = document.getElementById("resultDiv");
+const renderNumber = document.createElement('p');
 
 //create a way to convert any positive integer the user enters to a negative integer.
 
  function convertNumber(num) {
      num = document.getElementById("numberBox");
-     return -(Math.abs(num.value));
+     
+
+    renderNumber.innerText = '';
+    renderNumber.innerText = `${num.value}`;
+    parentDiv.append(renderNumber);
+    return -(Math.abs(num.value));
 }
 
- console.log(convertNumber(num.value));
+
+//create an event listener that starts the vowel count when the user clicks on the 'go' button.
+goButton.addEventListener("click", function() {
+    return convertNumber(num.value);
+});
+
 
 
